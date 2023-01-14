4ce84a07533d171c1900648b3841cb8629037047
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-01-14 20:45:30 +0000

    second project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonClose = document.querySelector('.close-modal');
+const buttonOpen = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < buttonOpen.length; i++) {
+  console.log(
+    buttonOpen[i].addEventListener('click', function () {
+      console.log('Button Clicked');
+      modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+    })
+  );
+}
+
+buttonClose.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
