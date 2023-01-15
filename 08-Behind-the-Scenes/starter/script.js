2325927e48dd0d90d686937662c89dc18ff319f9
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-01-15 20:06:10 +0000

    notes learning about scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+function callAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `you are ${age} years old, cause born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Gabriela';
+
+console.log(callAge(1998));
