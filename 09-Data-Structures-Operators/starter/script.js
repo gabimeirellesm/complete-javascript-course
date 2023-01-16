b54e95396851898e880d273f799e579eec948e95
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-01-16 19:55:27 +0000

    loop with for of

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -108,3 +108,52 @@ const loop = [1, 2, 3, 4, 5, 6];
 for (const numb of loop) {
   console.log(numb);
 }
+
+/* const obbj = {
+  name: 'gabi',
+  hobbies: {
+    morning: 'gym',
+    afternoon: 'read',
+    night: 'walk',
+  },
+  friends: {
+    manu: 'new',
+    tata: 'old',
+  },
+};
+
+for (const activities of Object.keys(hobbies)) {
+  console.log(activities);
+} */
+
+/* const restaurant2 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
+
+ */
+
+const airline = 'Tap';
+const plane = 'A320';
