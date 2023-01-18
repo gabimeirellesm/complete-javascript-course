8535245fa36f7377fab29cac7272a626d8ecd5f1
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-01-18 15:34:46 +0000

    define default values in parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassangers, price) {
+  const booking = {
+    flightNum,
+    numPassangers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LM123');
